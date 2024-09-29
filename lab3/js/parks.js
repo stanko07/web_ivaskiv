7ce3471f5ae42430d198132155fd6080eb380192
stanko07 <150948918+stanko07@users.noreply.github.com>
2024-09-29 12:22:10 +0300

    added lab 3

---
+++ lab3/js/parks.js
@@ -0,0 +1,45 @@
+export const parks = [
+    {   id:1,
+        address: "вул. Центральна, 10",
+        size: 2500,
+        price: 50
+    },
+    {   
+        Id: 2,
+        address: "вул. Паркова, 23",
+        size: 152,
+        price: 25
+    },
+    {   id: 3,
+        address: "вул. Богдана Хмельницького, 32",
+        size: 169,
+        price: 16
+    },
+    {   id:4,
+        address: "вул. Краківська вулиця, 12",
+        size: 136,
+        price: 17
+    },
+    {   id:5,
+        address: "вул. Пекарська, 4",
+        size: 142,
+        price: 27
+    },
+    {    id:6,
+        address: "вул. Високий замок, 22",
+        size: 154,
+        price: 19
+    },
+    {
+        id:7,
+        address: "вул. Проспект Свободи, 2",
+        size: 139,
+        price: 19
+    },
+    { 
+        id:8,
+        address: "вул. Крушельниця, 17",
+        size: 142,
+        price: 20
+    },
+];
